14c16da0ee08adca45961a43ebac8eb39e44012e
jay <270115050@yoobeestudent.ac.nz>
2021-06-01 13:30:42 +1200

    first commit

---
+++ FileWithGit/FileWithGit.cpp
@@ -0,0 +1,24 @@
+// FileWithGit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Shoes {
+    string brand, material, color;
+    float size, price;
+
+    Shoes(string defStr = "", float defNum = 0) {
+        brand = defStr;
+        material = defStr;
+        color = defStr;
+        size = defNum;
+        price = defNum;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
